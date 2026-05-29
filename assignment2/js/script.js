0cdf575c38ed9df0cec327218d08e8b8d72e07bf
Saida120 <105627129@student.swin.edu.au>
2026-05-29 18:37:54 +0000

    Add booking forms

--- assignment2/js/script.js
+++ assignment2/js/script.js
@@ -1,1 +1,7 @@
-/* Shared script file reserved for later interface behaviour. */
+function highlightCurrentPage() {
+    var page = document.body.dataset.page;
+    document.querySelectorAll('.site-nav a').forEach(function(link) {
+        if (link.dataset.page === page) link.classList.add('active');
+    });
+}
+window.onload = highlightCurrentPage;
